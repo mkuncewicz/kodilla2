3759e65983f7a59e45685749483b95f333bbee50
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-10-14 18:41:45 +0200

    module 19.3 - task

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,15 +1,19 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
     List<Task> findAll();
 
-    Task findById(long taskId);
+    Task save(Task task);
+
+    Optional<Task> findById(long taskId);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +20,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(long taskId){
-        return repository.findById(taskId);
+    public Task getTask(long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(long id){
+        repository.deleteById(id);
     }
 }
