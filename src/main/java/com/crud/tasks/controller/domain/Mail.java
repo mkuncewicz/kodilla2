b3c68dc93b5e0864d0925b60fcf7567eb05e21b6
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-11-05 11:51:49 +0100

    23.3 - Task Poprawka

--- src/main/java/com/crud/tasks/controller/domain/Mail.java
+++ src/main/java/com/crud/tasks/controller/domain/Mail.java
@@ -9,5 +9,5 @@ public class Mail {
     private final String mailTo;
     private final String subject;
     private final String message;
-    private final ring toCC;
+    private final String toCC;
 }
--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -26,7 +26,7 @@ class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test Message", nl);
+        Mail mail = new Mail("test@test.com", "Test", "Test Message", null);
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
